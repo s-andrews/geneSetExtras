c91681c9e0c5ef79837131b8967250b7ac02754b
s-andrews <simon@proteo.me.uk>
2026-01-27 10:23:24 +0000

    Added first functions and started a vignette

---
+++ R/cluster_enrich_result.R
@@ -0,0 +1,47 @@
+#' Title
+#'
+#' @param enrich_result A clusterProfiler categorical enrichment results from enrichGO or similar
+#' @param cluster_threshold The tree height at which to create clusters. Higher values give more clusters
+#' @param min_fold_enrichment Fold Enrichment filter for which terms to cluster
+#' @param max_p_adjust Adjusted P value filter for which terms to cluster
+#' @param clust_method How cluster agglomeration method to use - see hclust for details
+#' @param min_cluster_size The smallest cluster term size to report
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cluster_enrich_result <- function(enrich_result, cluster_threshold=0.9, min_fold_enrichment=1, max_p_adjust=0.05, clust_method="average", min_cluster_size=2) {
+  enrich_result |>
+    dplyr::filter(FoldEnrichment>=min_fold_enrichment) |>
+    dplyr::filter(p.adjust<=max_p_adjust) |>
+    enrichplot::pairwise_termsim() -> termsim_results
+  
+  
+  stats::as.dist(1-termsim_results@termsim) |>
+    stats::hclust(method = clust_method) |>
+    stats::cutree(h=cluster_threshold) -> clusters
+  
+  tibble::tibble(
+    Description=names(clusters),
+    term_cluster=clusters
+  ) |>
+    dplyr::group_by(term_cluster) |>
+    dplyr::mutate(term_cluster_size=n()) |>
+    dplyr::filter(term_cluster_size >= min_cluster_size) -> clusters
+  
+  
+  enrich_result |>
+    tibble::as_tibble() |>
+    dplyr::inner_join(clusters) -> clusters
+  
+  clusters |>
+    dplyr::arrange(term_cluster_size,pvalue) |>
+    dplyr::group_by(term_cluster) |>
+    dplyr::slice(1) |>
+    dplyr::ungroup() |>
+    dplyr::select(ID,Description,term_cluster,term_cluster_size,everything()) -> final_data
+  
+  return(final_data)
+    
+}
---
+++ R/volcano_plot.R
@@ -0,0 +1,33 @@
+#' Plot a volcano plot of categorical enrichment results
+#'
+#' @param enrich_result A clusterProfiler categorical enrichment results from enrichGO or similar
+#' @param interactive Whether to return a traditional ggplot (FALSE) or interactive plotly (TRUE) plot
+#'
+#' @return A ggplot or plotly plot
+#' @export
+#'
+#' @examples
+volcano_plot <- function(enrich_result, interactive=FALSE) {
+  
+  enrich_result |>
+    dplyr::as_tibble() |>
+    dplyr::mutate(setSize=as.integer(stringr::str_replace(BgRatio,"/.*$",""))) |>
+    dplyr::mutate(significant = dplyr::if_else(p.adjust<0.05,"Significant","Non-Significant")) |>
+    dplyr::mutate(FDR_Phred = -10*log10(p.adjust)) |>
+    arrange(desc(p.adjust)) -> plot_data
+  
+  
+  plot_data |>
+    ggplot2::ggplot(aes(x=FoldEnrichment, y=FDR_Phred, size=setSize, ID=ID, Description=Description, colour=significant)) +
+    ggplot2::geom_point(pch=21) +
+    scale_colour_manual(values=c("grey","red2")) -> volcano_plot
+  
+  
+  if (interactive) {
+      return(plotly::ggplotly(volcano_plot, width=800, height=500))
+  }
+  else {
+    return(volcano_plot)
+  }
+  
+}
