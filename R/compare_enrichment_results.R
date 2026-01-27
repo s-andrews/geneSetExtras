bb0d22e887b8c822f6d0e2853230305e93e045ea
s-andrews <simon@proteo.me.uk>
2026-01-27 16:40:12 +0000

    Use list names for groups when performing differential enrichment

--- R/compare_enrichment_results.R
+++ R/compare_enrichment_results.R
@@ -12,6 +12,12 @@ compare_enrichment_results <- function(enrichment_results) {
   # For each one we need to extract out the IDs, Description and Gene and Background
   # Ratios and put them into a common tibble
 
+  group_names <- names(enrichment_results)
+  
+  if (is.null(group_names)) {
+    group_names <- paste0("group_",1:length(enrichment_results))
+  }
+  
   list() -> data_for_testing
   
   for (i in 1:length(enrichment_results)) {
@@ -20,11 +26,14 @@ compare_enrichment_results <- function(enrichment_results) {
       dplyr::select(ID,Description, GeneRatio, BgRatio) |>
       tidyr::separate(GeneRatio, into=c("GeneSuccess","GeneTotal"), sep="/", convert=TRUE) |>
       tidyr::separate(BgRatio, into=c("BgSuccess","BgTotal"), sep="/", convert=TRUE) |>
-      tibble::add_column(result_id=i) -> data_for_testing[[i]]
+      tibble::add_column(group_name=group_names[i]) -> data_for_testing[[i]]
   }
   
   do.call(dplyr::bind_rows, data_for_testing) -> data_for_testing
   
+  data_for_testing |>
+    distinct(ID,Description) -> annotation_data
+
   # We have a problem here which is that if enrichGO doesn't have any hits to a category
   # in its hit list (not the background) then it doesn't report the result, not matter
   # what the filters are set to.  We can therefore only test categories which appear
@@ -33,7 +42,23 @@ compare_enrichment_results <- function(enrichment_results) {
   # This *may* be a fixed problem - the devel version of enrichGO uses enrichit as the
   # back end and this looks like it might report 0 overlap hits.  Need to wait for that
   # to hit production and check again.
+
+  # For now we're going to kludge this 
   
+  data_for_testing |>
+    select(-Description) |>
+    complete(ID,group_name) |>
+    arrange(ID,group_name) |>
+    mutate(GeneSuccess = replace_na(GeneSuccess,0)) |>
+    group_by(group_name) |>
+    mutate(GeneTotal = replace_na(GeneTotal, GeneTotal[!is.na(GeneTotal)][1])) |>
+    mutate(BgTotal = replace_na(BgTotal, BgTotal[!is.na(BgTotal)][1]))  |>
+    ungroup() |>
+    group_by(ID) |>
+    mutate(BgSuccess = replace_na(BgSuccess, min(BgSuccess[!is.na(BgSuccess)]))) |>
+    ungroup() -> data_for_testing
+  
+    
   data_for_testing |>
     dplyr::group_by(ID) |>
     dplyr::filter(
@@ -41,15 +66,13 @@ compare_enrichment_results <- function(enrichment_results) {
     ) |>
     dplyr::ungroup() -> data_for_testing
   
-  data_for_testing |>
-    distinct(ID,Description) -> annotation_data
   
   # A bit more stuff to do
   
   data_for_testing |>
     dplyr::arrange(ID) |>
     dplyr::mutate(Gene_Fail = GeneTotal-GeneSuccess, Bg_Fail=BgTotal-BgSuccess) |>
-    dplyr::select(ID,result_id, GeneSuccess,Gene_Fail, BgSuccess,Bg_Fail)  |>
+    dplyr::select(ID,group_name, GeneSuccess,Gene_Fail, BgSuccess,Bg_Fail)  |>
     dplyr::rename(Gene_Success=GeneSuccess, Bg_Success=BgSuccess) |>
     tidyr::pivot_longer(
       cols=c(Gene_Success,Gene_Fail,Bg_Success,Bg_Fail),
@@ -61,7 +84,7 @@ compare_enrichment_results <- function(enrichment_results) {
       names_from=type,
       values_from=count
     ) |>
-    dplyr::mutate(result_id=factor(result_id)) -> data_for_testing
+    dplyr::mutate(group_name=factor(group_name)) -> data_for_testing
   
   data_for_testing |>
     dplyr::group_by(ID) |>
@@ -82,13 +105,13 @@ compare_enrichment_results <- function(enrichment_results) {
 perform_test <- function(data,id) {
 
   stats::glm (
-    cbind(Success,Fail) ~ result_id * group,
+    cbind(Success,Fail) ~ group_name * group,
     data = data,
     family=binomial()
   ) -> fit
   
   stats::glm (
-    cbind(Success,Fail) ~ result_id + group,
+    cbind(Success,Fail) ~ group_name + group,
     data = data,
     family=binomial()
   ) -> fit_no_interaction
@@ -97,7 +120,23 @@ perform_test <- function(data,id) {
   
   test_result$`Pr(>Chi)`[-1] -> p_value
   
-  return(tibble::tibble(pvalue=p_value))
+  data |>
+    dplyr::group_by(group_name) |>
+    tidyr::pivot_wider(
+      names_from=group,
+      values_from=c(Success,Fail)
+    ) |>
+    dplyr::mutate(FoldEnrichment = (Success_Gene/(Success_Gene+Fail_Gene))/(Success_Bg/(Success_Bg+Fail_Bg))) |>
+    dplyr::ungroup() |>
+    dplyr::select(group_name,FoldEnrichment) |>
+    tidyr::pivot_wider(
+      names_from=group_name,
+      values_from=FoldEnrichment,
+      names_prefix = "FoldEnrichment_"
+    ) |>
+    tibble::add_column(pvalue=p_value, .before = 1) -> results_data
+  
+  return(results_data)
   
 }
 
